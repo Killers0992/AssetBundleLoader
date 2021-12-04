1bcfaaa2b959a1be77b2ee0031c8cfa5b58e3579
Killers0992 <killers0992@gmail.com>
2021-12-05 00:34:00 +0100

    bump

--- AssetBundleLoader/MainClass.cs
+++ AssetBundleLoader/MainClass.cs
@@ -11,7 +11,7 @@ namespace AssetBundleLoader
         public override string Prefix { get; } = "assetbundleloader";
 
         public override Version RequiredExiledVersion { get; } = new Version(4, 0, 0);
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 1);
 
         public static string PluginPath;
 
